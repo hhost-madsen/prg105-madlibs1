3503aa822017e184b04362e726fe54d0120c50fa
hhost-madsen <hhost-madsen@users.noreply.github.com>
2016-08-25 09:55:02 -0400

    Add files via upload

---
+++ Madlibs1.py
@@ -0,0 +1,22 @@
+# Variable
+thing = "iPhone"
+adjective = "on roam"
+accessory = "case"
+display = "face"
+emotion = "lighted"
+action = "ring"
+spin = "rotate"
+screen = "display"
+icon = "emoji"
+
+# Poem
+print("I'm a little " + thing + ",")
+print("Short and " + adjective + ",")
+print("Here is my " + accessory + ",")
+print("Here is my " + display + ",")
+print("When I get all " + emotion + ",")
+print("Hear me " + action + ",")
+print("Tip me over and " + spin + " " + screen + ",")
+print("Here's an " + icon + " of what I can do,")
+print("I can turn my " + accessory + " into a " + display + ",")
+print("Tip me over and " + spin + " " + screen + ".")
